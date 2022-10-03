3648ea638a8df971b847bcb23da8002fa3221470
Arthur Santos <kosarthur@gmail.com>
2022-10-03 11:32:43 +0100

    first commit

---
+++ src/test/java/com/mbio/challenge/ChallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mbio.challenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
